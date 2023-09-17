53b5fed71bb2ae21760b8ca0b6ce1e89edf1c612
whh <whh@qq.com>
2023-09-17 16:31:38 +0800

    master push

--- src/main/java/com/whh/Demo/GItDemo.java
+++ src/main/java/com/whh/Demo/GItDemo.java
@@ -8,5 +8,6 @@ public class GItDemo {
         System.out.println("Hello Git4");
         System.out.println("Hello GIt5");
         System.out.println("Hello Dev");
+        System.out.println("Hrllo push");
     }
 }
