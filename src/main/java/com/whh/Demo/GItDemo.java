9492c6dd9d321d63566bf3ce2b5badedd460967b
whh <whh@qq.com>
2023-09-17 15:39:56 +0800

    第二次提交hadoopDemo

--- src/main/java/com/whh/Demo/GItDemo.java
+++ src/main/java/com/whh/Demo/GItDemo.java
@@ -3,5 +3,6 @@ package com.whh.Demo;
 public class GItDemo {
     public static void main(String[] args) {
         System.out.println("Hello Git");
+        System.out.println("Hello GIt2");
     }
 }
